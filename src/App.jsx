1a012118fa53b24e22397f111ee7a109fa0a7407
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-04-30 10:52:05 +0530

    create a footer and other components and used routing using browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './Components/Body'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
 
 
 function App() {
@@ -8,7 +10,18 @@ function App() {
   return (
     
      <div>
-      <Navbar/>
+      <BrowserRouter basename='/'>
+        <Routes>
+            <Route path="/" element={<Body/>} >
+             
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+
+            </Route>
+
+        </Routes>
+      </BrowserRouter>
+
      </div>
   )
 }
