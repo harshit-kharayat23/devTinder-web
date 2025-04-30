0d3d41264df99a6024cfef70e90f601f174bf03c
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-04-30 09:51:38 +0530

    Lets start our DevTinder frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    
+     <div>
+      <h1 className='font-bold text-4xl text-white'>Hello Harshit!</h1>
+     </div>
+  )
+}
+
+export default App
